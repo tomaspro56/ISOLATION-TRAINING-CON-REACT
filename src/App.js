98a68dde4ac8058e621d4b845bed1dc9f677c75e
Tomas Perez <tomaspro56@gmail.com>
2020-07-27 20:12:33 -0500

    Cree mi primer proyecto en react y modifique el app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>ISOLATION TRAINING</h1>
+       <table class="tabla">
+         <tr class="tr1">
+           <td><strong>Nombres</strong></td>
+           <td><strong>Apellidos</strong></td>
+           <td><strong>Correo</strong></td>
+           <td><strong>Celular</strong></td>
+         </tr>
+         <tr>
+           <td>Yeferson</td>
+           <td>Lopera Barrientos</td>
+           <td>www.yeferson20lb@gmail.com</td>
+           <td>123456789</td>
+         </tr>
+         <tr>
+           <td>Santiago</td>
+           <td>Ortiz Hernández</td>
+           <td>so4261427@gmail.com</td>
+           <td>123456789</td>
+         </tr>
+         <tr>
+           <td>Tomás</td>
+           <td>Pérez</td>
+           <td>tomaspro56@gmail.com</td>
+           <td>123456789</td>
+         </tr>
+         <tr>
+           <td>Yeison</td>
+           <td>Posada Bolívar</td>
+           <td>Yeisonposadabolivar@gmail.com</td>
+           <td>123456789</td>
+         </tr>
+       </table>
     </div>
   );
 }
